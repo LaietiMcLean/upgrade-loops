7f961ca080f4ea0315a6e7dae4715685f7c7e2ec
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2021-12-04 12:13:36 +0100

    Iteration#2

--- loops.js
+++ loops.js
@@ -16,3 +16,40 @@ const products = [
     }
 
     console.log("Los productos que contienen 'camisetas' son: " + camisetas);
+
+//Iteración 2
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+let isApproved; 
+let texto1 = "Aprobado"
+let texto2 = "Suspenso"
+let summ = 0;
+
+for (aprov in alumns){
+    summ=0;
+    if(alumns[aprov].T1 == true) {
+        summ++;
+    } if(alumns[aprov].T2 == true) {
+        summ++;
+    } if(alumns[aprov].T3 == true) {
+        summ++;
+    } if (summ >= 2) {
+        isApproved = true;
+        console.log(alumns[aprov].name + ": aprobado");
+    } if (summ < 2) { 
+        isApproved = false;
+        console.log(alumns[aprov].name + ": suspenso");
+    }  
+    
+} 
+
+
+
+
+
